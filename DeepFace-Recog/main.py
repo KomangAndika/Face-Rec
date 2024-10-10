c4a022509cc0fea37333d4c894551274b929968a
KomangAndika <kwira.santosa@gmail.com>
2024-10-10 14:54:04 +0700

    fix

--- DeepFace-Recog/main.py
+++ DeepFace-Recog/main.py
@@ -1,5 +1,5 @@
 from deepface import DeepFace
 import json
 
-result = DeepFace.verify(img1_path="/Users/komangandikawirasantosa/Face-Rec/DeepFace-Recog/Photos/Trump1.png",img2_path="/Users/komangandikawirasantosa/Face-Rec/DeepFace-Recog/Photos/TrumpMew1.png")
+result = DeepFace.verify(img1_path="/Users/komangandikawirasantosa/Face-Rec/DeepFace-Recog/Photos/Barron.png",img2_path="/Users/komangandikawirasantosa/Face-Rec/DeepFace-Recog/Photos/TrumpMew1.png")
 print(json.dumps(result,indent=2))
